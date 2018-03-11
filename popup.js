5dffae5eefd71f660929ffe8e3a81083542f999d
GillinedUp <myrreah@gmail.com>
2018-03-11 05:53:52 +0100

    changed input forms

--- popup.js
+++ popup.js
@@ -1,21 +1,21 @@
-function getCurrentTabHostname(callback) {
-    const queryInfo = {
-        active: true,
-        currentWindow: true
-    };
-    chrome.tabs.query(queryInfo, tabs => {
-        const tab = tabs[0];
-        const url = new URL(tab.url);
-        callback(url.hostname); // we call newTabOpen here
-    });
-}
-
-function showHostname(hostname) {
-    console.log(hostname);
-    const par = document.querySelector("#domainName");
-    par.textContent = hostname;
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-    getCurrentTabHostname(showHostname)
-});
+// function getCurrentTabHostname(callback) {
+//     const queryInfo = {
+//         active: true,
+//         currentWindow: true
+//     };
+//     chrome.tabs.query(queryInfo, tabs => {
+//         const tab = tabs[0];
+//         const url = new URL(tab.url);
+//         callback(url.hostname); // we call newTabOpen here
+//     });
+// }
+//
+// function showHostname(hostname) {
+//     console.log(hostname);
+//     const par = document.querySelector("#domainName");
+//     par.textContent = hostname;
+// }
+//
+// document.addEventListener("DOMContentLoaded", () => {
+//     getCurrentTabHostname(showHostname)
+// });
